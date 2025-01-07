b55cd053b2099ca6130bacde6e7d4897f8e63a46
Hrishikesh778 <hrishi778@gmail.com>
2025-01-07 19:55:06 +0530

    modules

---
+++ footer.js
@@ -0,0 +1,63 @@
+function footer(){
+    
+    return `
+        <div class="footblock">
+            <h3>ONLINE SHOPPING</h3><br><br>
+
+
+
+            <h4>Men</h4>
+            <h4>Women</h4>
+            <h4>Kids</h4>
+            <h4>Home & Living</h4>
+            <h4>Beauty</h4>
+            <h4>Gift Cards</h4>
+            <h4> Myntra Insider</h4>
+        </div>
+        <div class="footblock">
+            <h3>CUSTOMER POLICIES</h3><br><br>
+
+
+            <h4>Contact Us</h4>
+            <h4>FAQ</h4>
+            <h4>T&C</h4>
+            <h4>Terms Of Use</h4>
+            <h4>Track Orders</h4>
+            <h4>Shipping</h4>
+            <h4>Cancellation</h4>
+            <h4>Returns</h4>
+            <h4>Privacy policy</h4>
+            <h4>Grievance Officer</h4>
+        </div>
+        <div class="footblock" id="social">
+            <h3>KEEP IN TOUCH</h3><br><br>
+
+            <h4>
+                <div class="i1"><i class="fa-brands fa-x-twitter"></i> Twitter</div>
+            </h4>
+            <h4>
+                <div class="i1"><i class="fa-solid fa-rss"></i> RSS</div>
+            </h4>
+            <h4>
+                <div class="i1"><i class="fa-brands fa-facebook"></i> Facebook</div>
+            </h4>
+            <h4>
+                <div class="i1"><i class="fa-brands fa-instagram"></i> Instagram</div>
+            </h4>
+        </div>
+
+        <div class="footblock">
+            <h3>USEFUL LINKS</h3><br><br>
+
+
+            <h4>Blog</h4>
+            <h4>Careers</h4>
+            <h4>Site Map</h4>
+            <h4>Corporate Information</h4>
+            <h4>Whitehat</h4>
+            <h4>Cleartrip</h4>
+        </div>
+`
+}
+
+export default footer
---
+++ index.js
@@ -0,0 +1,5 @@
+import navbar from "./navbar.js";
+import footer from "./footer.js";
+
+document.getElementById('fdiv').innerHTML = navbar()
+document.getElementById('footer1').innerHTML = footer()
